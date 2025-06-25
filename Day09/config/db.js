e3a4a791431ee3f561c38e1cc1d765901e727307
Soham Sarkar <srutidhara153@gmail.com>
2025-06-25 23:41:14 +0530

    DAY 09

--- Day09/config/db.js
+++ Day09/config/db.js
@@ -1,8 +1,8 @@
-//Conection for backend to database
+//Conection for backend to database.
 require('dotenv').config()
 const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI
-//create a asynv finction to connect data 
+//create a asynv finction to connect data.
 const db=async()=>{
     try{
         await mongoose.connect(uri)
